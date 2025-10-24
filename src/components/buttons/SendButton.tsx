21c9b962354a50e0054832cf6d9643ff22918375
tbwksm <ksm@tobeway.com>
2025-10-24 09:30:34 +0900

    Reset Icon: png -> svg

--- src/components/buttons/SendButton.tsx
+++ src/components/buttons/SendButton.tsx
@@ -45,7 +45,12 @@ export const DeleteButton = (props: SendButtonProps) => {
       title="Reset Chat"
     >
       <Show when={!props.isLoading} fallback={<Spinner class="text-white" />}>
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGQSURBVHgB7VTLTcNAEJ1d2+KIJRKJGynB+XAmJaQD6IBwQwLiNYgzoYJQAlSQ5IpIDB2EExL44ALiHXb8gUhZbCOI4JAnrbW/eW81njcAa/w1WNGFytl0Dzh21MUOANZoDwEeaURzLsLLxvPH3d7EDc6bXikBu+vb5qZ0GWAXcoDA+oHXPCJyxlC8eS1WKJCQR0N16KhliMj6wOU4ELsjOq+KewfQOFT0B2lISGE0KSVQcSdX6ctnpsXbL6efaVjEtvBrEUZ+Rq4T4EuvP/ZrWVryyAlzKfcXyXVYEjA2pJtOb/LIs5xDAfjyBjrxhMlr+CG5FlX3AWnAL4HDiqERYDP6xgZbhYDKzW2ig20oiS13MkhTOygUAINlAl37ZLoDBaCy5qnhVFmLQoGg1xiT/ZVbbMuUozwRIrc25JDmFKMra32rEL5tYUSBcclK5QlD8v7rReOJ1vH/USlUpUqGtKnxBV6rruP6utkpERPLNbs5414o6uG3BDIk/YbcHRvQSYLYTFIxMHmXNcA1/i/eARk6pU1JgTB4AAAAAElFTkSuQmCC" />
+        <svg viewBox="0 0 24 24" width="24" height="24">
+          <path
+            fill={props.sendButtonColor || '#ffffff'}
+            d="M17.6498 6.35C16.1998 4.9 14.2098 4 11.9998 4C7.57977 4 4.00977 7.58 4.00977 12C4.00977 16.42 7.57977 20 11.9998 20C15.7298 20 18.8398 17.45 19.7298 14H17.6498C16.8298 16.33 14.6098 18 11.9998 18C8.68977 18 5.99977 15.31 5.99977 12C5.99977 8.69 8.68977 6 11.9998 6C13.6598 6 15.1398 6.69 16.2198 7.78L12.9998 11H19.9998V4L17.6498 6.35Z"
+          />
+        </svg>
       </Show>
     </button>
   );
